570ac134e422c62d336c96c9ddabec1814535908
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2023-03-25 19:37:23 +0100

    log name

--- para-paint/src/views/Home-page.vue
+++ para-paint/src/views/Home-page.vue
@@ -6,11 +6,31 @@ const store = useCredentialsStore();
 </script>
 
 <template>
-    <div>
-        <h1>Home</h1>
-        <p v-if="store.name">
-            {{ store.name }}
-        </p>
+    <div class="home">
+        <h1 class="home__title">Home</h1>
+        <p v-if="store.name" class="home__subtitle">{{ store.name }}</p>
+        <DrawPage class="home__draw-page" />
     </div>
-    <DrawPage />
 </template>
+
+<style scoped lang="scss">
+.home {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 2rem;
+}
+
+.home__title {
+    font-size: 2rem;
+}
+
+.home__subtitle {
+    margin-top: 1rem;
+    font-size: 1.2rem;
+}
+
+.home__draw-page {
+    margin-top: 2rem;
+}
+</style>
--- para-paint/src/views/Login-page.vue
+++ para-paint/src/views/Login-page.vue
@@ -1,18 +1,28 @@
 <template>
-    <div>
-        <h1>Login Page</h1>
-        <form @submit.prevent="login">
-            <label>
-                Email:
-                <input type="text" id="name" v-model="name" required />
+    <div class="login">
+        <h1 class="login__title">Login Page</h1>
+        <form v-if="!store.name" class="login__form" @submit.prevent="login">
+            <label class="login__label">
+                Name:
+                <input
+                    class="login__input"
+                    type="text"
+                    id="name"
+                    v-model="name"
+                    required
+                />
             </label>
-            <button type="submit">Login</button>
+            <button class="login__button" type="submit">Login</button>
         </form>
+        <div v-else>
+            <p>Logged in as {{ name }}</p>
+            <router-link to="/">Go to Home</router-link>
+        </div>
     </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { useCredentialsStore } from "../stores/credentialsStore.js";
 const store = useCredentialsStore();
 
@@ -20,14 +30,57 @@ const name = ref("");
 
 function login() {
     store.name = name.value;
-    // console.log("Logged in as", store.setName);
-    console.log("Logged in as", name.value);
-    // store.setName(email.value);
-    // Perform login logic here
-    // this.$router.push("/");
 }
+onMounted(() => {
+    name.value = store.name;
+});
 </script>
 
+<style scoped lang="scss">
+.login {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.login__title {
+    margin-top: 2rem;
+    font-size: 2rem;
+}
+
+.login__form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 2rem;
+}
+
+.login__label {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    font-size: 1.2rem;
+}
+
+.login__input {
+    margin-top: 0.5rem;
+    padding: 0.5rem;
+    font-size: 1.2rem;
+}
+
+.login__button {
+    margin-top: 1rem;
+    padding: 0.5rem 1rem;
+    font-size: 1.2rem;
+    color: #fff;
+    background-color: #333;
+    border: none;
+    cursor: pointer;
+    &:hover {
+        background-color: #666;
+    }
+}
+</style>
 <!-- <script setup>
 
 const name = "";
