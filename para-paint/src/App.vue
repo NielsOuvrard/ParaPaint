6e7c0700883959864d85f1500528d15b702eabed
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2023-03-25 14:40:34 +0100

    work flow

--- para-paint/src/App.vue
+++ para-paint/src/App.vue
@@ -1,23 +1,36 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <TryDraw />
+    <div class="header">
+        <div class="header_h1">
+            <h1>ParaPaint</h1>
+        </div>
+    </div>
+    <div class="try_draw">
+        <TryDraw
+            width="1200"
+            height="600"
+            saveAs="png"
+            canvasId="TryDrawCanvas"
+        />
+        <div></div>
+    </div>
+    <div class="footer">
+        <p>© Niels Ouvrard, Florian Griffon, Lenny Delgado</p>
+        <p>Epitech Jam</p>
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import TryDraw from "./components/TryDraw.vue";
 
 export default {
     name: "App",
     components: {
-        HelloWorld,
         TryDraw,
     },
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -25,5 +38,44 @@ export default {
     text-align: center;
     color: #2c3e58;
     margin-top: 60px;
+}
+.header {
+    width: 100%;
+    background-color: rgb(62, 131, 214);
+    border-bottom: 2px solid;
+    border-color: rgb(215, 213, 213);
+}
+.header_h1 {
+    color: rgb(255, 255, 255);
+    font-size: larger;
+    font-weight: bolder;
+    text-align: left;
+    padding: 16px;
+    padding-left: 32px;
+}
+.try_draw {
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 64px;
+    margin-bottom: 64px;
+    width: 1200px;
+    height: 600px;
+    border: 4px solid;
+    border-color: rgb(215, 213, 213);
+}
+.footer {
+    width: 100%;
+    position: absolute;
+    bottom: 0px;
+    left: 0px;
+    padding-left: 32px;
+    padding-right: 32px;
+    background-color: rgb(62, 131, 214);
+    color: rgb(255, 255, 255);
+    font-size: medium;
+    font-weight: 700;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
 }
 </style>
