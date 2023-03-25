3a39b482e036044dbd49010df52c640e47ea4654
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2023-03-25 19:17:13 +0100

    rewrite header

--- para-paint/src/App.vue
+++ para-paint/src/App.vue
@@ -1,38 +1,10 @@
 <script setup>
-const routes = [
-    {
-        path: "/",
-        component: () => import("./views/Home-page.vue"),
-    },
-    {
-        path: "/about",
-        component: () => import("./views/About-page.vue"),
-    },
-    {
-        path: "/login",
-        component: () => import("./views/Login-page.vue"),
-    },
-    // {
-    //     path: "/draw",
-    //     component: () => import("./views/Draw-page.vue"),
-    // },
-    // {
-    //     path: "/:pathMatch(.*)*",
-    //     component: () => import("./views/404-page.vue"),
-    // },
-];
+import myHeader from "./components/Header.vue";
 </script>
 
 <template>
     <div id="app">
-        <header class="header">
-            <h1>ParaPaint</h1>
-            <div class="header__h1">
-                <nav v-for="route in routes" :key="route.path">
-                    <router-link :to="route.path">{{ route.path }}</router-link>
-                </nav>
-            </div>
-        </header>
+        <myHeader />
         <main>
             <router-view></router-view>
         </main>
@@ -50,21 +22,5 @@ const routes = [
     text-align: center;
     color: #2c3e58;
     // margin-top: 60px;
-}
-.header {
-    width: 100%;
-    background-color: rgb(62, 131, 214);
-    border-bottom: 2px solid;
-    border-color: rgb(215, 213, 213);
-    display: flex;
-    .__h1 {
-        height: 100%;
-        color: rgb(255, 255, 255);
-        font-size: larger;
-        font-weight: bolder;
-        // text-align: left;
-        padding: 16px;
-        padding-left: 32px;
-    }
 }
 </style>
---
+++ para-paint/src/components/Header.vue
@@ -0,0 +1,72 @@
+<script setup>
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: () => import("../views/Home-page.vue"),
+    },
+    {
+        path: "/about",
+        name: "About",
+        component: () => import("../views/About-page.vue"),
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: () => import("../views/Login-page.vue"),
+    },
+    // {
+    //     path: "/draw",
+    //     component: () => import("../views/Draw-page.vue"),
+    // },
+    // {
+    //     path: "/:pathMatch(.*)*",
+    //     component: () => import("../views/404-page.vue"),
+    // },
+];
+</script>
+
+<template>
+    <header class="header">
+        <h1 class="header__title">ParaPaint</h1>
+        <nav class="header__nav">
+            <router-link
+                v-for="route in routes"
+                :key="route.path"
+                class="header__link"
+                :to="route.path"
+            >
+                {{ route.name }}
+            </router-link>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss">
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    background-color: #333;
+    color: #fff;
+    background-color: rgb(62, 131, 214);
+}
+
+.header__title {
+    margin: 0;
+}
+
+.header__nav {
+    display: flex;
+}
+
+.header__link {
+    margin-left: 1rem;
+    color: #fff;
+    text-decoration: none;
+    &:hover {
+        text-decoration: underline;
+    }
+}
+</style>
