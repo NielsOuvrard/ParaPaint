2e58b8a2169f23080a594e25d515211fdbc76886
Florian Griffon <florian.griffon@epitech.eu>
2023-03-26 16:37:37 +0200

    Socket io server

--- para-paint/src/main.js
+++ para-paint/src/main.js
@@ -2,9 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/router";
 import { createPinia } from "pinia";
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
 
 const pinia = createPinia();
 const app = createApp(App);
+
+app.config.globalProperties.$socket = socket;
 
 app.use(router);
 app.use(pinia);
---
+++ para-paint/src/views/BgSave.vue
@@ -0,0 +1,14 @@
+<script setup>
+import FormComponent from "../components/FormComponent.vue";
+
+</script>
+
+<template>
+  <div>
+    <FormComponent />
+  </div>
+</template>
+
+<style>
+
+</style>
